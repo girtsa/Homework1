4001d86f6d29203218511192308e17b7be5a8847
Ģirts Alksnis <girtsa@inbox.lv>
2021-09-08 22:17:43 +0300

    Initial Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,28 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // ----------------------------------------
+
+            Console.WriteLine("Kā tevi sauc?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Sveiks, " + userName);
+
+
+            // ----------------------------------------------
+
+            int age = 15;
+            bool isAdult = false;
+
+            Console.WriteLine("Tavs vecums ir "+ +age +", tu esi pieaugušais"+ isAdult);
+
+
+            // ----------------------------------------------------------------------------------
+
+
+
         }
     }
 }
